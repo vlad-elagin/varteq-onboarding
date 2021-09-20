3518c230912222648d0e95d419a882b226b65e31
Vlad Elagin <vlad.elagin.fl@gmail.com>
2021-09-20 20:01:56 +0300

    compose

--- tasks/day1.ts
+++ tasks/day1.ts
@@ -32,4 +32,59 @@ describe("Day 1", () => {
     expect(curriedSum5(3, 3, 4)(5, 2)).toBe(17);
     expect(curriedSum5("a", "b", "c")("d", "e")).toBe("abcde");
   });
+
+  it("Task 2: implement compose", () => {
+    function compose<Type>(...fns: Function[]): (arg: Type) => Type {
+      return (arg) => {
+        let result = arg;
+        fns.reverse().forEach((fn) => {
+          result = fn(result);
+        });
+
+        return result;
+      };
+    }
+
+    const toLower = (text: string) => text.toLowerCase();
+    const toWordsArray = (text: string) => text.split(" ");
+    const toUpperShortWords = (words: string[]) =>
+      words.map((w) => (w.length > 5 ? w : w.toUpperCase()));
+    const toString = (words: string[]) => words.join(" ");
+
+    const composed = compose<string>(
+      toString,
+      toUpperShortWords,
+      toWordsArray,
+      toLower
+    );
+
+    expect(composed("loRem iPsum dOloRRrr")).toBe("LOREM IPSUM dolorrrr");
+  });
+
+  it("Task 3: implement composeWhileNotNil", () => {
+    function composeWhileNotNil(...fns: Function[]) {
+      return (arg) => {
+        let result = arg;
+        fns.reverse().forEach((fn) => {
+          if (result === undefined || result === null) {
+            return;
+          }
+
+          result = fn(result);
+        });
+
+        return result;
+      };
+    }
+
+    const toLower = (text: string) => text.toLowerCase();
+    const getProperty = (propName: string) => (obj: Record<string, any>) =>
+      obj[propName];
+
+    const composed = composeWhileNotNil(toLower, getProperty("name"));
+
+    expect(composed({ name: "JOHN" })).toBe("john");
+    expect(() => composed("age")).not.toThrow();
+    expect(composed("age")).toBeUndefined();
+  });
 });
